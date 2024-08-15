7afb30af5275181b92600a9c7bd429265e2548b2
kenyounot123 <kenlu519@gmail.com>
2024-08-15 16:35:08 -0400

    Update initializing firebase analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported  } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +18,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase Analytics only if the environment supports it
+if (typeof window !== "undefined") {
+  isSupported().then(supported => {
+    if (supported) {
+      const analytics = getAnalytics(app);
+    } else {
+      console.warn("Firebase Analytics is not supported in this environment.");
+    }
+  }).catch(error => {
+    console.error("Error initializing Firebase Analytics:", error);
+  });
+}
+
 export { db };
